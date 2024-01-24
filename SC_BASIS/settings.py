7dbe8eb1758094a254443eb0374f9e5172e50d92
Anthea Guo <anthea.jin.guo@gmail.com>
2024-01-23 17:49:24 -0800

    added url to requirements and imported missing module in settings

--- SC_BASIS/settings.py
+++ SC_BASIS/settings.py
@@ -16,6 +16,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 #BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
